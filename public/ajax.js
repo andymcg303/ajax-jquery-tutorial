5e677b9ec75036f241fd303bdc73f4ee764ce602
andymcg303 <mcgow_a@hotmail.com>
2019-11-14 10:16:06 +0000

    added put and delete requests

--- public/ajax.js
+++ public/ajax.js
@@ -1,12 +1,37 @@
-$.get('/todos', (data) => {
-    // alert("what a plonker");
-});
+// $.get('/todos', (data) => {
+//     // alert("what a plonker");
+// });
+
+// $('form').submit(function(e) {
+//     e.preventDefault();
+//     let formData = $(this).serialize();
+//     $.post('/todos', formData), function(data){
+//         console.log(data);
+//     };
+// });
+
+// $('form').submit(function(e) {
+//     e.preventDefault();
+//     let formData = $(this).serialize();
+//     let formAction =$(this).attr('action');
+//     $.ajax({
+//         url: formAction,
+//         data: formData,
+//         type: 'PUT',
+//         success: (data) => {
+//             console.log(data);
+//         }
+//     });
+// });
 
 $('form').submit(function(e) {
-    debugger;
     e.preventDefault();
-    let formData = $(this).serialize();
-    $.post('/todos', formData), function(data){
-        console.log(data);
-    };
+    let formAction =$(this).attr('action');
+    $.ajax({
+        url: formAction,
+        type: 'DELETE',
+        success: (data) => {
+            debugger;
+        }
+    });
 });
